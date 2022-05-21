0b087fe6f39d4133449b1d1b83d6fafa6af6e225
Ricardo <limaricardop@gmail.com>
2022-05-21 11:37:18 -0300

    Crud Routes added

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
+import usersRoute from "./routes/users.route";
+import statusRoute from "./routes/status.code";
 
 const app = express();
 
-app.use("/status", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: "bar Teste" });
-});
+//Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+
+//Configurões de Rotas
+app.use(usersRoute);
+
+app.use(statusRoute);
+
+
+//Inicialização do Servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000');
 });
